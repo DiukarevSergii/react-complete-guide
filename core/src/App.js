507c8731991643bc53bbe11c6f2acf53cf3c5ac0
Diukarev <diukarev.sergii@gmail.com>
2019-09-26 12:04:23 +0300

    refactored to list

--- core/src/App.js
+++ core/src/App.js
@@ -35,25 +35,22 @@ class App extends Component {
   };
 
   render () {
+      const persons = (
+          <div>
+          {this.state.persons.map((person, index) => {
+          return <Person name={person.name} age={person.age} personIndex={index}/>
+            })
+          }
+          </div>
+      );
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')}
-          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age} />
+          {persons}
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
