1a3b075cf107a186e9cf6278ee5330b12315e1d9
Diukarev <diukarev.sergii@gmail.com>
2019-09-27 10:31:35 +0300

    remove unexpected props

--- core/src/Person/Person.js
+++ core/src/Person/Person.js
@@ -4,7 +4,7 @@ import './Person.css';
 const person = ( props ) => {
     return (
         <div className="Person">
-            <p onClick={props.click} personIndex={props.personIndex}>I'm {props.name} and I am {props.age} years old!</p>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
         </div>
