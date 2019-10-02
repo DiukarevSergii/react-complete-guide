26b7d6a548811ba8305c2c79aff8cdb824e0db94
Diukarev <diukarev.sergii@gmail.com>
2019-10-02 10:45:25 +0300

    styling button

--- core/src/App.js
+++ core/src/App.js
@@ -94,7 +94,7 @@ class App extends Component {
         <div className="App">
             <h1>Hi, I'm a React App</h1>
             <p>This is really working!</p>
-            <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch/Reset Names</button>
+            <button className="Red" onClick={() => this.switchNameHandler('Maximilian!!')}>Switch/Reset Names</button>
             <br/>
             <input type="text" onChange={(event) => this.lineChangeHandler(event)}/>
             <br/>
