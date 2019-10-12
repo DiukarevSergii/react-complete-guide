c19c9809ecea83b783d508eb60db345a79cfcdc9
Diukarev <diukarev.sergii@gmail.com>
2019-10-12 17:27:55 +0300

    register service worker

--- core/src/index.js
+++ core/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
 
-const appTitle = "Persons manager";
-
-ReactDOM.render(<App title={appTitle}/>, document.getElementById('root'));
+ReactDOM.render(<App appTitle="Person Manager" />, document.getElementById('root'));
+registerServiceWorker();
