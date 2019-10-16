bd5aff402bf651f2a6ce7d07c2ba15f65590ed6a
Diukarev <diukarev.sergii@gmail.com>
2019-10-16 09:22:25 +0300

    add PropTypes

--- core/src/components/Persons/Person/Person.js
+++ core/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
@@ -23,5 +24,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.propTypes = {
+ click: PropTypes.func,
+ name: PropTypes.string,
+ age: PropTypes.number,
+ changes: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
